8c2a3679dd5ab46288ff6cc6d81e53c75b4eca39
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-06 14:40:02 -0600

    Modified version of the if file
    if/elseif/else

--- php/if.php
+++ php/if.php
@@ -1,39 +1,33 @@
 <?php 
      $a = 5;     // declare variable $a
      $b = 10;    // declare variable $b 
-     $c = '10';  // declare variable $c
+     $c = '15';  // declare variable $c
 
      if ( $a < $b) {
-     	echo "$a is lesser than $b\n" ;    //print the message
+     	echo "$a is less than $b\n" ;    //show this message if the condition is true
+     } else{
+
+        echo "$a is greater than $b\n";   //otherwise show this
      }
 
-     if  ($b > $a) {
-     	echo "$b is grater than $a\n" ;    //print the message
+
+      if  ($b >= $c) {
+     	echo "$b is greater or equal to $c\n" ;
+     }else{
+
+         echo "$b is less or equal to $c\n" ;
      }
 
-     if  ($b >= $c) {
-     	echo "$b is grater or equal to $c\n" ;
-     }
-
-      if  ($b <= $c) {
-     	echo "$b is lesser or equal to $c\n" ;
-     }
 
       if  ($b == $c) {
-     	echo "$b value is equal to $c\n" ;
+     	echo "$b is equal to $c\n" ;
+     }elseif ($b === $c){
+        echo "$b is identical to $c\n" ;
+     }elseif ($b !== $c){
+        echo "$b not identical to $c\n";
+     }else{
+        echo "$b is not equal to $c\n";
      }
-       if  ($b === $c) {
-     	echo "$b value and data type is equal to $c\n" ;
-     }
-
-      if  ($b != $c) {
-     	echo "$b is not equal to $c\n" ;
-     }
-
-     if ($b !== $c){
-     	echo "$b is not identical to $c\n";
-     }
-
 
 
 ?>
