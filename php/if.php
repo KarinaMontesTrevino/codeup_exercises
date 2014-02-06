26c408add3cec09082e0460518f0563db0b9d7e8
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-06 11:50:09 -0600

    added if file
    exercise in class

---
+++ php/if.php
@@ -0,0 +1,39 @@
+<?php 
+     $a = 5;     // declare variable $a
+     $b = 10;    // declare variable $b 
+     $c = '10';  // declare variable $c
+
+     if ( $a < $b) {
+     	echo "$a is lesser than $b\n" ;    //print the message
+     }
+
+     if  ($b > $a) {
+     	echo "$b is grater than $a\n" ;    //print the message
+     }
+
+     if  ($b >= $c) {
+     	echo "$b is grater or equal to $c\n" ;
+     }
+
+      if  ($b <= $c) {
+     	echo "$b is lesser or equal to $c\n" ;
+     }
+
+      if  ($b == $c) {
+     	echo "$b value is equal to $c\n" ;
+     }
+       if  ($b === $c) {
+     	echo "$b value and data type is equal to $c\n" ;
+     }
+
+      if  ($b != $c) {
+     	echo "$b is not equal to $c\n" ;
+     }
+
+     if ($b !== $c){
+     	echo "$b is not identical to $c\n";
+     }
+
+
+
+?>
