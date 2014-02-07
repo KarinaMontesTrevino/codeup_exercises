3d3ef340e545346ef2aca09018557a77898e7038
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-07 16:55:36 -0600

    for.php fourth step

--- php/for.php
+++ php/for.php
@@ -1,5 +1,21 @@
 <?php
 
+// test if the arguments passed are 4 if not exit the program
+   if ($argc !==4){
+
+    echo "Not the right number of arguments passed, try again!\n"; // tell the user that the arguments passed should be 3
+     exit(0);
+   }
+
+   $first_argument = $argv[1];
+   $second_argument = $argv[2];
+   $third_argument = $argv[3];
+
+   $first_argument = is_numeric($argv[1]) ?  intval($argv[1]) : 1;
+   $second_argument= is_numeric($argv[2]) ?  intval($argv[2]) : 2; 
+   $second_argument= is_numeric($argv[3]) ?  intval($argv[3]) : 3;  
+
+
 //prompts user to choose a start number
   fwrite(STDOUT, "Choose a start number\n");
   
@@ -19,12 +35,19 @@
   //store input into the ending_number
   $incrementor_number = intval(trim(fgets(STDIN)));
 
+  if ($incrementor_number == 0){
+
+  	 $incrementor_number = 1;
+
+  }
+
+ //var_dump($incrementor_number);
  
  //loop that shows all the sequence from the start to the ending number
-  for($counter = $start_number; $counter <= $ending_number; $counter += $incrementor_number){
+    for($counter = $start_number; $counter <= $ending_number; $counter += $incrementor_number){
     
-     echo "\n$counter\n";
-
+      echo "\n$counter\n";
+  
   }
 
 ?>
