9627d9bcf71110e74befcf5d0e313a6582f71cc2
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-07 16:18:05 -0600

    for.php second step

--- php/for.php
+++ php/for.php
@@ -9,11 +9,19 @@
  //prompts user to choose the ending number
   fwrite(STDOUT, "Choose an ending number\n");
   
- //allocate the input from user in a variable 
+ //store input into the ending_number
   $ending_number = intval(trim(fgets(STDIN)));
+
+
+  //prompts user to guess the random number
+  fwrite(STDOUT, "Choose an incrementor number\n");
+  
+  //store input into the ending_number
+  $incrementor_number = intval(trim(fgets(STDIN)));
+
  
  //loop that shows all the sequence from the start to the ending number
-  for($counter = $start_number; $counter <= $ending_number; $counter++){
+  for($counter = $start_number; $counter <= $ending_number; $counter += $incrementor_number){
     
      echo "\n$counter\n";
 
