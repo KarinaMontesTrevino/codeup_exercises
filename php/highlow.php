7d5c2581c352c7aa236d1169d83422d8be1ef128
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-07 13:14:38 -0600

    Guess Game

--- php/highlow.php
+++ php/highlow.php
@@ -17,19 +17,19 @@ do{
  //add loop number to counter
   $counter ++;   
 
-  if($guess_number == $random_number){
+  if($guess_number == $random_number){  //test if the guess is the same number as the random number
     
     echo "Congratulations, YOU WIN!\n";  //if user guess is correct tell them he/she wins
   
   } elseif ($guess_number < $random_number){
   	
-    echo "Please guess higher\n"; // if user guess number is lower tell them to guess higher
+    echo "Please guess higher\n"; //if user guess number is lower tell them to guess higher
   } else{
-    echo "please guess lower\n"; // if the user guess number is higher tell them to guess lower
+    echo "please guess lower\n"; //if the user guess number is higher tell them to guess lower
   }
 
 }while($guess_number != $random_number); //do this until the user guess the right number
-echo "Number of guesses: $counter\n\n";// number of times the user guessed
+echo "Number of guesses: $counter\n\n";//number of times the user guessed
 
 exit(0); //exit the program 
  
