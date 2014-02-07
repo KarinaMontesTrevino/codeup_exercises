61ca203ae3e68d1b4dd29664fce94c31aa195996
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-07 13:07:10 -0600

    Guess game counting # of guesses

--- php/highlow.php
+++ php/highlow.php
@@ -3,28 +3,34 @@
   //pick a random  number
 
    $random_number = mt_rand (1, 100);
+   $counter = 0;
 
 do{
 
   //prompts user to guess the random number
-  fwrite(STDOUT, 'Guess a number between 1 and 100 ? ');
+  fwrite(STDOUT, 'Guess a number between 1 and 100 ');
   
  //allocate the input from user in a variable 
-     $guess_number = fgets(STDIN);
-
-
+  $guess_number = fgets(STDIN);
+   
+ //add loop number to counter
+  $counter ++;   
 
   if($guess_number == $random_number){
-    echo "Congratulations, YOU WIN!\n\n";  //if user guess is correct tell them he/she wins
+    
+    echo "Congratulations, YOU WIN!\n";  //if user guess is correct tell them he/she wins
+  
   } elseif ($guess_number < $random_number){
-  	echo "Please guess higher\n\n"; // if user guess number is lower tell them to guess higher
+  	
+    echo "Please guess higher\n"; // if user guess number is lower tell them to guess higher
   } else{
-    echo "please guess lower\n\n"; // if the user guess number is higher tell them to guess lower
+    echo "please guess lower\n"; // if the user guess number is higher tell them to guess lower
   }
 
-exit(0); //exit the program
 }while($guess_number != $random_number); //do this until the user guess the right number
+echo "Number of guesses: $counter\n\n";
 
-
+exit(0); //exit the program 
+ 
 
 ?>
