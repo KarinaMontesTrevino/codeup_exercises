66760e7fcba557b06426abe1177ce3e0d8bd01ff
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-07 16:14:51 -0600

    for.php first step

--- php/for.php
+++ php/for.php
@@ -1,17 +1,18 @@
 <?php
 
-//prompts user to guess the random number
+//prompts user to choose a start number
   fwrite(STDOUT, "Choose a start number\n");
   
- //allocate the input from user in a variable 
+ //store input into the start_number variable
   $start_number = intval(trim(fgets(STDIN)));
 
- //prompts user to guess the random number
+ //prompts user to choose the ending number
   fwrite(STDOUT, "Choose an ending number\n");
   
  //allocate the input from user in a variable 
   $ending_number = intval(trim(fgets(STDIN)));
  
+ //loop that shows all the sequence from the start to the ending number
   for($counter = $start_number; $counter <= $ending_number; $counter++){
     
      echo "\n$counter\n";
