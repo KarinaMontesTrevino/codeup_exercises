256e59081349746aafb373cefcded8dc846eeb82
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-07 13:26:39 -0600

    Highlow.php

--- php/highlow.php
+++ php/highlow.php
@@ -15,21 +15,25 @@ do{
   $guess_number = fgets(STDIN);
    
  //add loop number to counter
-  $counter ++;   
+  $count_guesses ++;   
 
-  if($guess_number == $random_number){  //test if the guess is the same number as the random number
+    if($guess_number == $random_number){  //test if the guess is the same number as the random number
     
-    echo "Congratulations, YOU WIN!\n";  //if user guess is correct tell them he/she wins
+        echo "Congratulations, YOU WIN!\n";  //if user guess is correct tell them he/she wins
   
-  } elseif ($guess_number < $random_number){
+    } elseif ($guess_number < $random_number){
   	
-    echo "Please guess higher\n"; //if user guess number is lower tell them to guess higher
-  } else{
-    echo "please guess lower\n"; //if the user guess number is higher tell them to guess lower
-  }
+        echo "Please guess higher\n"; //if user guess number is lower tell them to guess higher
+  
+    } else{
+  
+        echo "please guess lower\n"; //if the user guess number is higher tell them to guess lower
+     
+    }
 
 }while($guess_number != $random_number); //do this until the user guess the right number
-echo "Number of guesses: $count_guesses\n\n";//number of times the user guessed
+
+    echo "Number of guesses: $count_guesses\n\n";//number of times the user guessed
 
 exit(0); //exit the program 
  
