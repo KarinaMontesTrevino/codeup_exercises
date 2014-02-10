420410efd29433e93080967a314f89319d4aa747
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-10 15:03:44 -0600

    break_continue.php 1-10

--- php/break_continue.php
+++ php/break_continue.php
@@ -2,8 +2,8 @@
 
 // Program that creates another for loop that counts from 1 to 100, but stops after 10 using break.
 for ($i = 1; $i <= 100; $i++) {
-     if ($i % 2!= 0) {
-        continue;
+     if ($i == 11) {
+        break;
       }
     echo $i . "\n";
 }
