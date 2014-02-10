0353d56561454bac512dec4a2c15e5d333851e13
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-10 09:13:14 -0600

    fizzbuzz.php

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -5,17 +5,17 @@
 for ($number= 1; $number <=100; $number++){
 
   
-		//check if the number is remainder of BOTH 3 and 5 and print FizzBuzz
+		//check if the number is divisable of BOTH 3 and 5 and print FizzBuzz
         if($number % 3 == 0 && $number % 5 == 0){
 
   	        echo "FizzBuzz \n";
 
-        //check if the number is remainder of 3 and print fizz
+        //check if the number is divisable by 3 and print fizz
         }elseif ($number % 3 == 0){
 
 	        echo "Fizz\n";
 
-        // check if the number is remainder of 5 and print buzz
+        // check if the number is divisable by 5 and print buzz
         }elseif ($number % 5 == 0){
 		
 		   echo "Buzz\n";
