4466e98cf6d842d71ef01dfa36c4a9f92d0bad30
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-10 11:17:03 -0600

    foreach.php
    echo out every value

---
+++ php/foreach.php
@@ -0,0 +1,50 @@
+<?php
+// the following program will check the data type of every element of our array and it will print the appropiate message.
+
+    // define array called things that contains different types of data
+   $things = array ('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+   //  iterate through each element of the array called things and assign it to $element 
+   foreach ($things as $element){
+
+   // if the element in the array is integer then print the message
+   		if (is_integer($element)){
+
+   			echo "{$element} is an integer\n";
+
+   // if the element in the array is float then print the message
+   		}elseif (is_float($element)){
+          
+            echo "{$element} is a float\n";
+
+   // if the element in the array is boolean then don't print anything       
+   		}elseif (is_bool($element)){
+
+   			echo "\n"; 
+   
+   // if the element in the array is an array prin the content of that array
+   		}elseif (is_array($element)){
+
+   			// iterates through our second array and prints its content	
+   			foreach ($element as $sub_element) {
+           
+               echo "{$sub_element}";
+   				
+   			}// close second foreach
+
+            echo "\n";
+    
+    // if the element in the array is null then don't print anything
+   		}elseif (is_null($element)){
+   			echo "\n";
+ 
+    // if the element in the array is  strin then print the message
+   		}elseif (is_string($element)){
+   			echo "{$element} is a string\n";
+   		}
+
+  
+   }//close main foreach
+
+
+?> 
