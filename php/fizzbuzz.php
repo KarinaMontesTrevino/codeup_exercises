686896c97f3132328f3fda525cecaee71e6a6700
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-10 09:08:56 -0600

    fizzbuzz

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -1,30 +1,34 @@
 <?php
 
+
+// loop that will count from 1 to 100
 for ($number= 1; $number <=100; $number++){
 
   
+		//check if the number is remainder of BOTH 3 and 5 and print FizzBuzz
+        if($number % 3 == 0 && $number % 5 == 0){
 
-  if($number % 3 == 0 && $number % 5 == 0){
+  	        echo "FizzBuzz \n";
 
-  	echo "FizzBuzz \n";
+        //check if the number is remainder of 3 and print fizz
+        }elseif ($number % 3 == 0){
 
-  }elseif ($number % 3 == 0){
+	        echo "Fizz\n";
 
-	  echo "Fizz\n";
-
-  }elseif ($number % 5 == 0){
+        // check if the number is remainder of 5 and print buzz
+        }elseif ($number % 5 == 0){
 		
 		   echo "Buzz\n";
 
-   }else{
+        // if nothing is true print only the number
+        }else{
 
         echo "$number\n";
          
-   }
-
-       
+        } //close if
+   
     
- }
+ } //close for
 
 
 ?>
