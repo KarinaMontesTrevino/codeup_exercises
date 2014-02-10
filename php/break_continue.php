d0b75b18442d9241a65db151032fbbd9829de2fe
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-10 14:40:18 -0600

    break_continue.php second step

--- php/break_continue.php
+++ php/break_continue.php
@@ -1,13 +1,11 @@
 <?php
 
-// Program that creates a for loop that shows all even numbers between 1 and 100 using continue
-
+// Program that creates another for loop that counts from 1 to 100, but stops after 10 using break.
 for ($i = 1; $i <= 100; $i++) {
     echo $i . "\n";
-    if ($i % 2 != 0) {
-        continue;
-    }
-    echo "^ that is an even number.\n";
+     if ($i == 10) {
+        break;
+      }
 }
 
 ?>
