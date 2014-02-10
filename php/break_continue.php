bb0f2cdeaa2440f2646a9c521e7858bf653bed43
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-10 14:59:03 -0600

    break_continue.php even numbers

--- php/break_continue.php
+++ php/break_continue.php
@@ -2,10 +2,10 @@
 
 // Program that creates another for loop that counts from 1 to 100, but stops after 10 using break.
 for ($i = 1; $i <= 100; $i++) {
-    echo $i . "\n";
-     if ($i == 10) {
-        break;
+     if ($i % 2!= 0) {
+        continue;
       }
+    echo $i . "\n";
 }
 
 ?>
