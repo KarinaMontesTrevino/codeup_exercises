e6ab6cfafc78215580a36e3bfd7407d7ce762313
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-13 15:50:20 -0600

    list_with_commas.php default sort

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -3,32 +3,29 @@
 // Converts array into list n1, n2, ..., and n3
 function humanized_list($get_input, $sorting = false) {
   // Your solution goes here!
-
+    $exploded = explode(', ', $get_input);
+     
 	// test if the user wants to sort 
 	if ($sorting){
 
 	//sorts the array in alphabetical order
-		sort($get_input);
+		sort($exploded);
          
     }
 
-  
     // pops the last element of the array passed and stores the element in this variable
-	$result = array_pop($get_input);
+	$last_element = array_pop($exploded);
 	// takes the array and converts it into a string adding the word "and" before the last element
-    $result = implode(', ', $get_input)." and ". $result;
-     // return the result
-	return $result;
+    return $imploded = implode(', ', $exploded)." and ". $last_element;
+     
 }
 
 // List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-// converts a string into an array
-$physicists_array = explode(', ', $physicists_string);
 
 
 // Humanize that list
-$famous_fake_physicists = humanized_list($physicists_array, true);
+$famous_fake_physicists = humanized_list($physicists_string, true);
 
 // Output sentence
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
