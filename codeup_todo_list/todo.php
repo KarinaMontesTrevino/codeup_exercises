459c85b41521c47961868c77fc72acca34d73db6
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-02-14 13:54:08 -0600

    todo.php  fwritr() overwriting

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -6,24 +6,21 @@ $items = array();
 // List array items formatted for CLI
 function list_items($items){
 
-    // declare variable array_converto_string
+    // Declare variable array_converto_string
      $array_converto_string = '';
     
     foreach ($items as $key => $item) {
 
-       $newIndex = $key + 1;     //insead of key++
+       $newIndex = $key + 1;     //instead of key++
        $array_converto_string .=  "[" . $newIndex . "]" . $item . PHP_EOL;  //same as $arrray_convert_string = $arrray_convert_string 
 
     }
-
-    return $array_converto_string;
-    
+    return $array_converto_string;   
 }
 
 // Get STDIN, strip whitespace and newlines, 
 // and convert to uppercase if $upper is true
 function get_input($upper = false) {
-
 
     if ($upper){   //if $upper ==true  or if there is anything in there                  
 
@@ -44,7 +41,7 @@ function open_file(){
     echo "Enter the path and the name of the file:  ";
 
     // Stores the input from user
-    $filename = get_input();
+    $filename = get_input(true);
 
     // Opens the file and indicates read only
     $handle = fopen($filename, "r");
@@ -60,15 +57,16 @@ function open_file(){
 }
 
 // Function that saves a file
-function write_file($array){
+function write_file($get_array){
     // Prompts the user to enter the path and name of the file
     echo "Enter the path and the name of the file:  ";
 
     // Stores the input from user
     $filename = get_input();
        
+    // Checks if the file already exists   
     if (file_exists($filename)){
-
+          
            echo "The file you are trying to save already exists, do you wish to continue (Y)es or (N):  ";
            $input = get_input(true);
 
@@ -77,23 +75,20 @@ function write_file($array){
                 // Opens the file and indicates to start at the beginning of the file and removes the content, or creates a new file if it does not exist 
                $handle = fopen($filename, 'w');
 
-                //iterates through our array of items and saves each element with a new line
-                foreach ($array as $item) {
-             
+                // Iterates through our array of items 
+                foreach ($get_array as $item) {
+                       // Saves each element with a new line
                        fwrite($handle, PHP_EOL . $item);
                  } 
                 // Alerts the user the file has been saved successfully      
                  echo "File Saved Sucessfully" . PHP_EOL; 
-                 // close the connection to the file 
+                 // Close the connection to the file 
                    fclose($handle);
            }else {
                  echo "Nothing has been saved, you canceled the operation" . PHP_EOL;
-           }// close if input 'Y'
-
-    }// Close the if file exists
- 
-        
-}// closes the function
+           }// Close if input 'Y'
+    }// Close the if file exists   
+}// Close the function
 
 
 // The loop!
@@ -149,31 +144,31 @@ do {
         }else{
             rsort($items);
         }
-    //special feature in the main menu that allows to remove the first item on the list
+    // Special feature in the main menu that allows to remove the first item on the list
     }elseif ($input == 'F'){
         // Prompt for confirmation 
         echo "Are you sure to remove the first item (Y)es or (N)o?  ";
         $input = get_input(true);
-        // if the user says yes it removes the first element of the list
+        // If the user says Yes it removes the first element of the list
         if ($input == 'Y'){ 
-          array_shift($items);
-          echo "Item sucessfully removed " . PHP_EOL;
-         // if the user says no nothing gets removed 
+           array_shift($items);
+           echo "Item sucessfully removed " . PHP_EOL;
+         // If the user says No nothing gets removed 
         }else{
-          echo "Don't worry, nothing has been removed" . PHP_EOL;
+           echo "Don't worry, nothing has been removed" . PHP_EOL;
         }
 
     //special feature in the main menu that allow to remove the last item on the list     
     }elseif ($input == 'L'){
         echo "Are you sure to remove the last item (Y)es or (N)o ?  ";
         $input = get_input(true);
-        // if the user says yes it removes the last element of the list
+        // If the user says Yes it removes the last element of the list
         if ($input == 'Y'){ 
-        array_pop($items);
-         echo "Item sucessfully removed " . PHP_EOL;
-         // if the user says no nothing gets removed 
+           array_pop($items);
+           echo "Item sucessfully removed " . PHP_EOL;
+         // If the user says No nothing gets removed 
         }else{
-          echo "Don't worry, nothing has been removed" . PHP_EOL;
+           echo "Don't worry, nothing has been removed" . PHP_EOL;
         }
 
     }elseif ($input == 'O'){
@@ -183,7 +178,7 @@ do {
         // add the new incoming array to the existing array 
         $items = array_merge($items,$content_array);
     }elseif ($input == 'SA'){
-        // Saves the file that the user indicated
+        // Calls the function write_file and passes an array to save it
          write_file($items);  
     }
 
